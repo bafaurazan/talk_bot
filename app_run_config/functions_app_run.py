8858cf0da047e0218370904ab94c4e52213be798
bafaurazan <rafalbe777@gmail.com>
2024-05-08 09:49:17 +0200

    changing installation process

--- app_run_config/functions_app_run.py
+++ app_run_config/functions_app_run.py
@@ -1,5 +1,6 @@
 """Functions for argument_parser_app_run.py"""
 
+import os
 import subprocess
 from pathlib import Path
 from .platform_dependency_app_run import shell_execute_type
@@ -11,26 +12,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 def api_preparing():
     """Crucial preparations for the API and Django operations"""
+    os.chdir(BASE_DIR / "api")
     subprocess.run(
-        [
-            "cd",
-            str(BASE_DIR / "api"),
-            "&&",
-            "poetry",
-            "install",
-            "&&",
-            "poetry",
-            "run",
-            "python",
-            "manage.py",
-            "makemigrations",
-            "&&",
-            "poetry",
-            "run",
-            "python",
-            "manage.py",
-            "migrate",
-        ],
+        ["poetry", "install"],
+        shell=shell_execute_type,
+        check=True,
+    )
+    os.chdir(BASE_DIR / "api")
+    subprocess.run(
+        ["poetry", "run", "python", "manage.py", "makemigrations"],
+        shell=shell_execute_type,
+        check=True,
+    )
+    os.chdir(BASE_DIR / "api")
+    subprocess.run(
+        ["poetry", "run", "python", "manage.py", "migrate"],
         shell=shell_execute_type,
         check=True,
     )
@@ -38,8 +34,9 @@ def api_preparing():
 
 def talk_bot_preparing():
     """Crucial preparations for talk_bot operation"""
+    os.chdir(BASE_DIR / "my_bot")
     subprocess.run(
-        ["cd", str(BASE_DIR / "my_bot"), "&&", "poetry", "install"],
+        ["poetry", "install"],
         shell=shell_execute_type,
         check=True,
     )
@@ -47,17 +44,9 @@ def talk_bot_preparing():
 
 def api_run():
     """Running django server"""
+    os.chdir(BASE_DIR / "api")
     subprocess.run(
-        [
-            "cd",
-            str(BASE_DIR / "api"),
-            "&&",
-            "poetry",
-            "run",
-            "python",
-            "manage.py",
-            "runserver",
-        ],
+        ["poetry", "run", "python", "manage.py", "runserver"],
         shell=shell_execute_type,
         check=True,
     )
@@ -65,16 +54,9 @@ def api_run():
 
 def talk_bot_run():
     """Running talk_bot"""
+    os.chdir(BASE_DIR / "my_bot/bot_config")
     subprocess.run(
-        [
-            "cd",
-            str(BASE_DIR / "my_bot/bot_config"),
-            "&&",
-            "poetry",
-            "run",
-            "python",
-            "bot.py",
-        ],
+        ["poetry", "run", "python", "bot.py"],
         shell=shell_execute_type,
         check=True,
     )
